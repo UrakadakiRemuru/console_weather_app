3bea9396acfb82fc761695f7de3102d50a36e09d
UrakadakiRemuru <met-zi@mail.ru>
2024-12-10 18:25:43 +0300

    upd responsefromopenweather model

--- core/weather_console/models.py
+++ core/weather_console/models.py
@@ -24,7 +24,6 @@ class RequestParamsToOpenWeather(models.Model):
 
 
 class ResponseFromOpenWeather(models.Model):
-    city = models.CharField(max_length=255)
     weather = models.CharField(max_length=255)
     temperature = models.IntegerField()
     feels_like = models.IntegerField()
