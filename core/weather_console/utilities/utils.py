a8dc8da7e10d94f7202dcfd881ce7324d81c5bfd
UrakadakiRemuru <met-zi@mail.ru>
2024-12-20 17:32:15 +0300

    added representations for weather form request

--- core/weather_console/utilities/utils.py
+++ core/weather_console/utilities/utils.py
@@ -1,5 +1,8 @@
-from googletrans import Translator
+from datetime import datetime
+from typing import Dict
 
+from googletrans import Translator
+from weather_console.services.prepare_data import METRICS_MAP
 
 def get_translator() -> Translator:
     '''
@@ -16,3 +19,42 @@ def get_translator() -> Translator:
     except ConnectionError as e:
         raise ConnectionError('Произошла ошибка подключения к сервису.'
                               ' Проверьте подключение к интернету и повторите попытку позже.') from e
+
+
+def prepare_weather_data_to_representation(weather_data: Dict[str, str | float | int],
+                                           coordinates: Dict[str, str | float | int],
+                                           units_code: str) -> Dict[str, str]:
+    '''
+    Добавляет название города из ответа от geocoding в отформатированный словарь ответа от OWM и возвращает измененный
+    словарь.
+
+    Args:
+        weather_data (Dict[str, str | float | int]): Отформатированный словарь ответа от OWM.
+        coordinates (Dict[str, str | float | int]): Отформатированный словарь ответа от geocoding.
+        units_code (str): Код системы единиц измерения.
+
+    Returns:
+        Словарь с данными о погоде.
+
+    Examples:
+        {
+            'city': ...,
+            'time': ...,
+            'temperature': ...,
+            'feels_like': ...,
+            'wind_speed': ...,
+        }
+    '''
+
+    temp = METRICS_MAP.get('temperature').get(units_code)
+    speed = METRICS_MAP.get('speed').get(units_code)
+
+    city_name = coordinates.get('city')
+    weather_data['temperature'] = str(weather_data['temperature']) + temp
+    weather_data['feels_like'] = str(weather_data['feels_like']) + temp
+    weather_data['wind_speed'] = str(weather_data['wind_speed']) + speed
+    return {
+        'city': city_name,
+        'time': datetime.now().strftime('%H:%M:%S %d.%m.%Y'),
+        **weather_data
+    }
