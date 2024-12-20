9fbb73c4afd28c1c520da5e44be313751b55110a
UrakadakiRemuru <met-zi@mail.ru>
2024-12-20 17:30:10 +0300

    added logic for working with geocoding api

--- core/weather_console/weather_api/geocoding_api.py
+++ core/weather_console/weather_api/geocoding_api.py
@@ -1,10 +1,10 @@
 import os
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import requests
 from dotenv import load_dotenv
 from googletrans import Translator
-
+from requests import exceptions
 from weather_console.weather_by_name.weather_by_name import translate_anything, get_translated_country_name_by_code
 
 load_dotenv()
@@ -12,11 +12,11 @@ load_dotenv()
 API_KEY = os.getenv('OWM_API_KEY')
 
 
-def get_city_coordinates(literal_name: Dict[str, str]) -> Dict[str, float | str | dict[str, str]]:
+def get_city_coordinates(names_map: Dict[str, str]) -> Dict[str, float | str | dict[str, str]]:
     '''
     Получение координат города через geocoding-api.
     Args:
-        literal_name (dict[str, str]): Словарь с наименованием города и кода страны или наименованием города.
+        names_map (dict[str, str]): Словарь с наименованием города и кода страны или наименованием города.
         {'city': val, 'country_code': val}
 
     Raises:
@@ -29,8 +29,8 @@ def get_city_coordinates(literal_name: Dict[str, str]) -> Dict[str, float | str 
 
     '''
 
-    city_name = literal_name.get('city')
-    country_code = literal_name.get('country_code') or ''
+    city_name = names_map.get('city')
+    country_code = names_map.get('country_code') or ''
 
     params = {
         'q': f'{city_name},{country_code}',
@@ -40,9 +40,9 @@ def get_city_coordinates(literal_name: Dict[str, str]) -> Dict[str, float | str 
 
     try:
         response = requests.get('http://api.openweathermap.org/geo/1.0/direct', params=params, timeout=5)
-    except ConnectionError as e:
+    except exceptions.ConnectionError as e:
         raise ConnectionError('Проверьте подключение к интернету и повторите попытку.') from e
-    except TimeoutError as e:
+    except (exceptions.Timeout, exceptions.ReadTimeout) as e:
         raise TimeoutError('Проблемы соединения с сервисом. Повторите попытку позже') from e
 
     if response.status_code == 200:
@@ -55,7 +55,43 @@ def get_city_coordinates(literal_name: Dict[str, str]) -> Dict[str, float | str 
     raise ConnectionError('Произошла проблема на стороне сервиса. Попробуйте повторить попытку позже.')
 
 
-def parse_response(response: List[Dict], lang_preference: str = 'ru', *, translator: Translator) -> List[
+def get_city_coordinates_reversed(lat: float, lon: float) -> Dict[str, float | str | dict[str, str]]:
+    '''
+    Получение ответа geocoding-api по координатам.
+
+    Args:
+        lat (float): Широта.
+        lon (float): Долгота.
+
+    Raises:
+        ConnectionError: В случае проблем подключения к интернету или проблем на стороне сервиса.
+        TimeoutError: В случае проблем подключения к сервису.
+
+    Returns:
+        Список словарей с названием города, кодом страны, а также широтой и долготой.
+
+    '''
+
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'limit': 1,
+        'appid': API_KEY
+    }
+
+    try:
+        response = requests.get('http://api.openweathermap.org/geo/1.0/reverse', params=params, timeout=5)
+    except exceptions.ConnectionError as e:
+        raise ConnectionError('Проверьте подключение к интернету и повторите попытку.') from e
+    except (exceptions.Timeout, exceptions.ReadTimeout) as e:
+        raise TimeoutError('Проблемы соединения с сервисом. Повторите попытку позже') from e
+
+    if response.status_code == 200:
+        return response.json()
+
+    raise ConnectionError('Произошла проблема на стороне сервиса. Попробуйте повторить попытку позже.')
+
+def parse_geocoding_response(response: List[Dict], lang_preference: str, *, translator: Translator) -> List[
     Dict[str, float | str | None]]:
     '''
 
@@ -68,18 +104,23 @@ def parse_response(response: List[Dict], lang_preference: str = 'ru', *, transla
         Список словарей следующего формата:
         [
             {
-                'name': название города (str),
+                'city': название города (str),
                 'state': область (str)
                 'country': название страны (str),
+                'country_code': код страны в ISO-3166 (str),
                 'lat': широта (float),
                 'lon': долгота (float)
             }
         ]
     '''
-
-    lang_preference = lang_preference.lower()
+    if lang_preference:
+        lang_preference = lang_preference.lower()
+    else:
+        lang_preference = 'ru'
 
     parsed_list = []
+
+    static_city_name = ''
 
     for city_dict in response:
         current_city_info = {}
@@ -88,18 +129,26 @@ def parse_response(response: List[Dict], lang_preference: str = 'ru', *, transla
         if not local_names:
             continue
 
-        current_city_info['name'] = local_names.get(lang_preference)
+        city_name = local_names.get(lang_preference)
+
+        if city_name:
+            static_city_name = city_name
+
+        current_city_info['city'] = static_city_name
+
+        country_code = city_dict.get('country')
 
         current_city_info.update({
-            'country_code': get_translated_country_name_by_code(city_dict.get('country'), lang_preference,
-                                                                translator=translator),
+            'country': get_translated_country_name_by_code(country_code, lang_preference,
+                                                           translator=translator),
+            'country_code': country_code,
             'lat': city_dict.get('lat'),
             'lon': city_dict.get('lon'),
         })
 
         state = city_dict.get('state')
-        if state == city_dict.get('name'):
-            state = current_city_info['name']
+        if state == city_dict.get('city'):
+            state = current_city_info['city']
         elif state:
             state = translate_anything(state, lang_preference, translator=translator)
 
@@ -108,3 +157,23 @@ def parse_response(response: List[Dict], lang_preference: str = 'ru', *, transla
         parsed_list.append(current_city_info)
 
     return parsed_list
+
+
+def get_coordinates_from_parsed_geocoding_response(parsed_geocoding_response: Dict[str, float | str | None]) -> Tuple[
+    float,
+    float]:
+
+    '''
+    Позволяет получить широту и долготу из данных о городе из ответа geocoding.
+
+    Args:
+        parsed_geocoding_response (Dict[str, float | str | None]): Данные о городе из ответа geocoding.
+
+    Returns:
+        Кортеж с координатами (широта, долгота).
+    '''
+
+    return (
+        parsed_geocoding_response.get('lat'),
+        parsed_geocoding_response.get('lon')
+    )
