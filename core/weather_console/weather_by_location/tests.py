a649cc48c69c2d87a69498e2539ff14087b805e9
UrakadakiRemuru <met-zi@mail.ru>
2024-12-20 17:07:26 +0300

    deleted files

--- core/weather_console/weather_by_location/tests.py
+++
@@ -1,8 +0,0 @@
-
-if __name__ == '__main__':
-    from .weather_by_location import get_latitude_and_longitude
-    from weather_console.weather_api.openweathermap_api import get_weather_data
-    coords = get_latitude_and_longitude()
-    print(coords)
-    data = get_weather_data(coords.get('lat'), coords.get('lon'))
-    print(data)
