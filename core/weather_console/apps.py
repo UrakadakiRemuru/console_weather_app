840b9aa4fe815c53a3da37154c6ebbe3f8b94865
UrakadakiRemuru <met-zi@mail.ru>
2024-12-20 17:08:25 +0300

    added preferences instance creation on start up

--- core/weather_console/apps.py
+++ core/weather_console/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 
 class WeatherConsoleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather_console'
+
+    def ready(self):
+        from .models import UserPreferences
+
+        @receiver(post_migrate)
+        def create_user_preferences(sender, **kwargs):
+            if not UserPreferences.objects.exists():
+                UserPreferences.objects.create(is_first_time=True)
